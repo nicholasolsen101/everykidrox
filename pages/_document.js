c8f3e52580c11468b18453644dd73332b502e9e4
@nicholas.olsen <nicholas.olsen@diply.com>
2022-11-06 01:27:10 -0500

    favicons

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,13 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#2463eb" />
+          <meta name="msapplication-TileColor" content="#2463eb" />
+          <meta name="theme-color" content="#ffffff" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
           <link href="https://fonts.googleapis.com/css2?family=Lilita+One&family=Roboto+Slab:wght@400;600&family=Source+Sans+Pro:wght@400;600&display=swap" rel="stylesheet" />
